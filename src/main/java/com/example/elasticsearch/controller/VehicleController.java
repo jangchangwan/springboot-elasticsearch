8b793a936f8bfe1b1e427ead98b8c5204d286df1
ChangWan12 <changwa327@mindslab.ai>
2023-04-26 14:59:12 +0900

    add Vehicle

---
+++ src/main/java/com/example/elasticsearch/controller/VehicleController.java
@@ -0,0 +1,25 @@
+package com.example.elasticsearch.controller;
+
+import com.example.elasticsearch.document.Vehicle;
+import com.example.elasticsearch.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/vehicle")
+public class VehicleController {
+    private final VehicleService service;
+
+    @Autowired
+    public VehicleController(VehicleService service) { this.service = service; };
+
+    @PostMapping
+    public void index(@RequestBody final Vehicle vehicle) {
+        service.index(vehicle);
+    }
+
+    @GetMapping("/{id}")
+    public Vehicle getById(@PathVariable final String id) {
+        return service.getById(id);
+    }
+}
---
+++ src/main/java/com/example/elasticsearch/document/Vehicle.java
@@ -0,0 +1,21 @@
+package com.example.elasticsearch.document;
+
+public class Vehicle {
+    private String id;
+    private String number;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
